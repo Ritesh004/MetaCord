c5e1052aa2537c686916064959022bc6556eac11
Ritesh004 <120500548+Ritesh004@users.noreply.github.com>
2025-05-27 23:58:20 +0530

    contract deployment

--- test/Dappcord.js
+++ test/Dappcord.js
@@ -5,5 +5,36 @@ const tokens = (n) => {
 }
 
 describe("Dappcord", function () {
+  let deployer, user
+  let dappcord
+    const NAME = "Dappcord"
+    const SYMBOL = "DC"
+
+    this.beforeEach(async () => {
+      [deployer, user] = await ethers.getSigners()
+      // Deploy contract
+        const Dappcord = await ethers.getContractFactory("Dappcord")
+        dappcord = await Dappcord.deploy(NAME, SYMBOL) 
+    })
+    describe("Deployment", function(){
+      it("Sets the name", async () => {
+        // Fetch name
+        let result = await dappcord.name()
+        // check name
+        expect(result).to.equal(NAME)
+      })
+
+       it("Sets the symbol", async () => {
+        // Fetch symbol
+        let result = await dappcord.symbol()
+        // check symbol
+        expect(result).to.equal(SYMBOL)
+       })
+
+       it("Sets the owner", async () => {
+        const result = await dappcord.owner()
+        expect(result).to.equal(deployer.address)
+       })
+    })
 
 })
