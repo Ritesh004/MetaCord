a2419a1962b85d4b4a23426101a724ed17bdb06d
Ritesh004 <120500548+Ritesh004@users.noreply.github.com>
2025-06-25 00:45:14 +0530

    messages sending

--- src/components/Messages.js
+++ src/components/Messages.js
@@ -7,73 +7,45 @@ import send from '../assets/send.svg'
 
 // Socket
 const socket = io('ws://localhost:3030')
+const Messages =({account, messages, currentChannel}) => {
+  const [message, setMessage]= useState("")
 
-const Messages = ({ account, currentChannel }) => {
-  const [messages, setMessages] = useState([])
-  const [message, setMessage] = useState('')
-  const messagesEndRef = useRef(null)
-
-  const scrollToBottom = () => {
-    messagesEndRef.current?.scrollIntoView({ behavior: "smooth" })
+const messagesEndRef = useRef(null);
+useEffect(() => {
+    // Scroll to the bottom of the messages when they change
+    messagesEndRef.current?.scrollIntoView({ behavior: "smooth" });
+  }, [messages]);
+  const sendMessage = async (e) => {
+    e.preventDefault();
+    console.log("sending message...", message);
   }
-
-  useEffect(() => {
-    socket.emit('get messages')
-    socket.on('get messages', (messages) => {
-      setMessages(messages)
-    })
-    socket.on('new message', (messages) => {
-      setMessages(messages)
-    })
-  }, [])
-
-  useEffect(() => {
-    scrollToBottom()
-  }, [messages])
-
-  const sendMessage = (e) => {
-    e.preventDefault()
-    if (message) {
-      const msg = {
-        channel: currentChannel.id,
-        account: account,
-        text: message
-      }
-      socket.emit('new message', msg)
-      setMessage('')
-    }
-  }
-
   return (
     <div className="text">
       <div className="messages">
-        {messages
-          .filter(message => message.channel === currentChannel?.id)
-          .map((message, index) => (
-            <div key={index} className="message">
-              <img src={person} alt="Person" />
-              <div className="message_content">
-                <h3>{message.account}</h3>
-                <p>{message.text}</p>
-              </div>
+        
+        {currentChannel && messages.filter(message => message.channel === currentChannel.id.toString()).map((message, index) => (
+          <div className="message" key={index}>
+            <img src = {person} alt= "Person"/>
+            <div className="message_content">
+              <h3>{message.account.slice(0,6)+'...'+ message.account.slice(38,42)}</h3>
+              <p>
+                {message.text}
+              </p>
             </div>
-          ))}
+          </div>
+        ))}
         <div ref={messagesEndRef} />
       </div>
 
-      <form onSubmit={sendMessage}>
-        <input
-          type="text"
-          placeholder={`Message #${currentChannel?.name}`}
-          value={message}
-          onChange={(e) => setMessage(e.target.value)}
-        />
-        <button type="submit">
-          <img src={send} alt="Send" />
-        </button>
-      </form>
+        <form onSubmit={sendMessage}>
+          <input type="text" onChange={(e)=> setMessage(e.target.value)}></input>
+          <button type="submit">
+            <img src={send} alt="Send Message"/>
+          </button>
+        </form>
+
     </div>
   );
 }
 
-export default Messages;
+export default Messages; 
